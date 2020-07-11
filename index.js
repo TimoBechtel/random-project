f63c98fdc67fc9dafbf1327a01284f4b2b14e31e
Timo Bechtel <timo@bechtel.solutions>
2020-07-11 23:40:37 +0200

    feat: init

---
+++ index.js
@@ -0,0 +1,17 @@
+// https://github.com/CompactJS/random
+const pick = (choices) => {
+	return choices[Math.floor(Math.random() * choices.length)];
+};
+
+const fetchRepos = async () => {
+	const res = await fetch('https://api.github.com/users/TimoBechtel/repos');
+	const repos = await res.json();
+	return repos;
+};
+
+const redirectToRandomRepo = async () => {
+	const repos = await fetchRepos();
+	window.location.replace(pick(repos).html_url);
+};
+
+redirectToRandomRepo();
